ad34217ba083e729e1fae836c31bfb23c6e167dc
root <oreajileye58@gmail.com>
2022-03-10 09:38:06 -0800

    fifth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,35 +1,15 @@
-#include <stdio.h>
-
-
-
-
+#include  <stdio.h>
 
 /**
- *
- *  * main - Where programs are executed
- *
- *   *
- *
- *    * Return: 0
- *
- *     */
-
-
+ * main - Where programs are executed
+ * 
+ *  Return: 0
+ */
 
 int main(void)
-	
-	
-	
+
 {
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
-
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-
-
-			return (0);
-
-
-
-}}
+}
